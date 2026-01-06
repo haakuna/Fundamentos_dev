7205d4bf0acebd490efb149edbf61cc992583564
haakuna <arturo.matata@gmail.com>
2026-01-05 22:34:32 -0500

    if else

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,18 +1,3 @@
-let firstName: string = "Kevin";
-let lastName: string = "Medina";
 
-let fullName: string = firstName + " " + lastName;
-console.log(fullName);
 
-let numer1: number = 10;
-let number2: number = 20;
-
-console.log(numer1 + number2);
-
-let isActivo: boolean = true;
-let isCorre: boolean = false;
-let hasNino: boolean = true;
-
-console.log('Esta activo?', isActivo);
-console.log('corre?', isCorre);
-console.log('es niño?', hasNino);
+console.log("Inicio");
