ef856a1a0f88434e7d97d2ffaa7e7ee147b03fd0
haakuna <arturo.matata@gmail.com>
2026-04-21 14:04:33 -0500

    tareas

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,40 +1,23 @@
 
-class Car {
-    readonly marca: string;
-    puertas: number;
-    tanque: number;
-    isActive: boolean;
-    tipo: string;
-    fechaCreacion: number;
 
-    constructor( marca: string, tipo: string ){
-        this.marca = marca;
-        this.puertas = 0;
-        this.tanque = 100;
-        this.isActive = false;
-        this.tipo = tipo;
-        this.fechaCreacion = 123456789;
+let numbers = [1, 6, 8, 4, 2, 7, 10, 3, 5 ]; 
+// cual es el numero mayor
+let max = numbers[0];
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+        max = numbers[i];
     }
+}
+console.log("El número mayor es: " + max);
 
-    turnOn(){
-        if (this.isActive){
-            console.log ('el carro se malogro F');
-            return;
-        }
-
-        if (this.tanque <= 0){
-            console.log ('El tanque esta vacio');
-            return;
-
-        }
-        this.isActive = true;
-        console.log('El carro se encendio');
+let numeros = [1, 6, 8, 4, 2, 7, 10, 3, 5 ];
+//identificar cual es par e impar
+for (let i = 0; i <= numeros.length; i++) {
+    let numero = numeros[i];
+    if (numero % 2 === 0) {
+        console.log(numero + " es un número par.");
+    } else {
+        console.log(numero + " es un número impar.");
     }
 
 }
-
-let miCarro = new Car('lambo', 'deportivo');
-
-console.log(miCarro);
-miCarro.turnOn();
-console.log(miCarro);
